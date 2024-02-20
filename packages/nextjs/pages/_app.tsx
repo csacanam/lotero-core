cbbc83de7e8ab43e6310534aa11585a51a5e4963
Camilo Sacanamboy <csacanam@gmail.com>
2024-02-20 00:17:12 -0500

    Changed the position of the play button

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -41,7 +41,7 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
         avatar={BlockieAvatar}
         theme={isDarkTheme ? darkTheme() : lightTheme()}
       >
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen mainUIComponent">
           <Header />
           <main className="relative flex flex-col flex-1">
             <Component {...pageProps} />
